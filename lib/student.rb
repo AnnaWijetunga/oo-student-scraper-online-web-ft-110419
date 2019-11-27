cca9161a178936f4909992538ca3d0bd416fd54d
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-27 18:05:03 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,30 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |attribute, value|
+      self.send("#{attribute}=", value)
+      # use meta-programming to assign the newly created student attributes and values per the key/value pairs of the hash
+    end
+    @@all << self # add the newly created student to the student class' @@all array of all students 
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student_hash|
+      Student.new(student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |attr, value|
+      self.send("#{attr}=", value)
+    end
+    # iterate over given hash
+    # use meta-programming to dynamically assign the student attributes and values per the key/value pairs of the hash 
+    self # return value should be the student 
   end
 
   def self.all
-    
+    @@all 
   end
 end
 
